76d5056df2ff5c1862a7ef70c255d6ce6a174f0e
svidya <svidya1725@gmail.com>
2016-03-29 00:22:16 +0100

    final Changes

--- UserModule.interfaces/IUserModulePresenter.cs
+++ UserModule.interfaces/IUserModulePresenter.cs
@@ -1,13 +1,11 @@
 ﻿namespace UserModule.Interfaces
 {
     public interface IUserModulePresenter
-    {   
-       
-        bool UpdateUserData(string userProfileId, string userProfileDomainName, string userProfileName, string userProfileAccount, bool isAdmin, string userProfileMailAddress,long operatorId);
-        long RegisterUserProfile(string userName);  
-        string GetOperatorName(long operatorId);
-
+    {
+        bool UpdateUserData(string userProfileId, string userProfileDomainName, string userProfileName, string userProfileAccount, bool isAdmin, string userProfileMailAddress, long operatorId);
+        long RegisterUserProfile(string userName);
+        string GetOperatorName(long operatorId);  
         bool DeleteUserData(string userProfileId);
-        
+
     }
 }
